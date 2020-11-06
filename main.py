a37d8564e8162b7c04e466cc7919c0d54892dbc5
Andrew Gregorovic <andrew.g.93@hotmail.com>
2020-11-06 11:38:53 +1100

    split out multiple files

--- main.py
+++ main.py
@@ -1,18 +1,8 @@
 from flask import Flask, request, jsonify, abort
 app = Flask(__name__)
-import psycopg2
-
-connection = psycopg2.connect(
-    database="library_api",
-    user="app",
-    password="Testing1",
-    host="localhost"
-)
-
-cursor = connection.cursor()
-
-cursor.execute("create table if not exists books (id serial PRIMARY KEY, title varchar);")
-connection.commit()
+from database import connection, cursor
+from dotenv import load_dotenv
+load_dotenv()
 
 @app.route("/books", methods=["GET"])
 def book_index():
